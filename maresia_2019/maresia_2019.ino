add5691b38e6049ce1583b21460c8681c651944f
Ailton Oliveira <45828689+AiltonOliveir@users.noreply.github.com>
2019-08-06 23:11:26 -0300

    Frequency and hc12 suport

--- maresia_2019/maresia_2019.ino
+++ maresia_2019/maresia_2019.ino
@@ -1,45 +1,41 @@
 // --- Mapeamento de Hardware ---
 // arduino uno ou nano
 //Ponte H Bts7960
-//modulo de sensoriamento Nrf24l01 PA
+//modulo de sensoriamento HC-12
 
-#include <SPI.h> //INCLUSÃO DE BIBLIOTECA
-#include <nRF24L01.h> //INCLUSÃO DE BIBLIOTECA
-#include <RF24.h> //INCLUSÃO DE BIBLIOTECA
 
-#define volt 2    // voltimetro no pino 2
-#define ch3   3   // Canal 3 no pino 3
-#define ch5   4   // Canal 5 no pino 4
+
+#define ch3   4   // Canal 3 no pino 3
+#define ch5   5   // Canal 5 no pino 4
 #define r_en   7   // Right Enable 1 no pino 7
 #define l_en   8   // Left Enable 2 no pino 8
-#define r_pwm  9  // Right PWM  no pino 9
-#define l_pwm  10 // Left PWM no pino 10
+#define r_pwm  3  // Right PWM  no pino 9
+#define l_pwm  11 // Left PWM no pino 10
 
-
-
-RF24 radio(5, 6); //CRIA UMA INSTÂNCIA UTILIZANDO OS PINOS (CE, CSN)
-const byte address[6] = "00666";
+#include <SoftwareSerial.h>
+SoftwareSerial HC12(9, 10); // HC-12 TX Pin, HC-12 RX Pin
 
 void read_channels();      //Função para leitura das entradas dos canais
 void test_channels();      //Função que mostra na tela os valores que o arduino recebe do controle remoto
 void avante();
 void reverso();
 void sensoriamento();
+void setFrequency(char option);     //Seleciona a frequência de operação do PWM
 
 //variáveis para os canais do rádio
 int canal_03 = 0,canal_05 = 0;
 int PWM=0;
-int tensao = 0;
+float tensao = A2;
+
 
 void setup()
 {
   pinMode(ch3, INPUT); //Entrada para o canal 3 do rádio
   pinMode(ch5, INPUT); //Entrada para o canal 5 do rádio
 
-  radio.begin(); //INICIALIZA A COMUNICAÇÃO SEM FIO
-  radio.openWritingPipe(address); //DEFINE O ENDEREÇO PARA ENVIO DE DADOS AO RECEPTOR
-  radio.setPALevel(RF24_PA_HIGH); //DEFINE O NÍVEL DO AMPLIFICADOR DE POTÊNCIA
-  radio.stopListening(); //DEFINE O MÓDULO COMO TRANSMISSOR (NÃO RECEBE DADOS)
+  TCCR2A = 0xA3;         //Configura operação em fast PWM, utilizando registradores OCR2x para comparação
+                         //1010 0011
+  setFrequency(2);       //Seleciona opção 4 para frequência PWM (aprox 7.8kHz)
 
  //controlarão o PWM
   pinMode(r_pwm, OUTPUT);
@@ -65,7 +61,7 @@ void loop()
       digitalWrite(r_en,HIGH);
       digitalWrite(l_en,HIGH);      
       read_channels();
-      // test_channels();
+      test_channels();
        
 while(canal_05<1500) // enquanto a chave estiver para baixo
 {
@@ -106,8 +102,8 @@ void read_channels()
     
 } //end read channels
 void sensoriamento(){
-  tensao = analogRead(volt)*(12/1023);
-  radio.write(&tensao, sizeof(int)); //ENVIA AO RECEPTOR A MENSAGEM
+  volt = analogRead(tensao);
+  HC12.write(volt);
   delay(5);
 }
 void avante()
@@ -135,3 +131,24 @@ void test_channels() //Testa os canais via serial monitor (comentar esta funçã
 
 
 } //end test_channels
+
+void setFrequency(char option)
+{
+  /*
+  TABLE:
+  
+      option  frequency
+        
+        1      62.5  kHz
+        2       7.81 kHz
+        3       1.95 kHz
+        4     976.56  Hz
+        5     488.28  Hz
+        6     244.14  Hz
+        7      61.03  Hz   
+  */
+  TCCR2B = option;
+  
+   
+
+} //end setFrequency
