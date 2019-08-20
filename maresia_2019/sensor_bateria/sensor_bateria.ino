dd2d7b31fc393551663c84c0f1e28bdc7a8a4a14
Ailton Oliveira <45828689+AiltonOliveir@users.noreply.github.com>
2019-08-20 12:02:38 -0300

    Update sensor_bateria.ino

--- maresia_2019/sensor_bateria/sensor_bateria.ino
+++ maresia_2019/sensor_bateria/sensor_bateria.ino
@@ -1,18 +1,36 @@
 #include <SoftwareSerial.h>
-SoftwareSerial HC12(3, 2); // HC-12 TX Pin, HC-12 RX Pin
-float fator_correcao = (12.6/1023.0);
-float valor, tensao;
+#include <LiquidCrystal.h>
+SoftwareSerial HC12(8, 9); // HC-12 TX Pin, HC-12 RX Pin
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+float tensao = 12.6;
+float antena = 0;
 
 
 void setup() {
-  Serial.begin(9600); //INICIALIZA A SERIAL
-  HC12.begin(9600);               // Serial port to HC12
- 
-void loop() {
-  while (HC12.available()) {       // If HC-12 has data
-      valor = HC12.read();
-       if ( valor != -1.0){
-       Serial.print(valor);      // Send the data to Serial monitor
-       Serial.println("volts");
-  }
+  Serial.begin(9600);             
+  HC12.begin(9600);               
+  lcd.begin(16, 2);
+  
 }
+
+void loop() {
+  while (HC12.available()) {        
+    if ( valor != -1.0){
+    antena = HC12.read();          
+    tensao = antena*(12.6/255.0);
+    }}
+  
+   //Limpa a tela
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(0, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("Robotech-Maresia");
+  lcd.setCursor(4, 1);
+  lcd.print(tensao);
+  lcd.setCursor(9, 1);
+  lcd.print("V");
+  delay(500);
+    
+  }
